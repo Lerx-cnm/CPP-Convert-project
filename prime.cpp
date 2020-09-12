9d99417236221f8c171307855d7aa00968bffe40
steedn <steedn@outlook.com>
2020-09-12 14:34:28 -0600

    first commit

---
+++ prime.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  int num = 0;
+
+  cout << "Please enter a number to check if it is a prime number: ";
+  cin >> num;
+  cout << num << "\n";
+}
