436f61c4945797c719f0455f32a486d37773bf6f
steedn <steedn@outlook.com>
2020-09-12 15:37:58 -0600

    started to implement a loop feature

--- convert.cpp
+++ convert.cpp
@@ -1,6 +1,16 @@
 #include <cmath>
 #include <iostream>
 using namespace std;
+void check(){
+  cout << "Would you like to do another conversion?(y/n)\n";
+  string input;
+  cin >> input;
+  if(input == "y"){
+    main();
+  }
+  else{
+  }
+}
 
 int main(){
   double temp = 0;
@@ -14,15 +24,20 @@ int main(){
       double temp1;
       temp1 = (temp - 32)/1.8;
       cout << "The temperature is " << round(temp1) << " degrees\n";
+      check();
     }
   else if (input == "c")
     {
       cout << "Please enter a temperature to convert co farenheit: ";
       cin >> temp;
       double temp1;
-      
+      temp1 = (temp*1.8)+32;
+      cout << "The temperature is " << round(temp1) << " degrees\n";
+      check();
     }
   else
     {
+      cout << "Please enter a valid answer\n";
+      main();
     }
 }
